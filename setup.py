c0d08b5610df54a056d6d4548f21f14cc18e7cdc
Robin Westin <robin.westin@visma.com>
2017-10-13 11:21:28 +0200

    added requirement

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="HelloWold",
     version="1.0",
     packages=find_packages(),
+    install_requires=["requests"],
     entry_points={
         "console_scripts": [
             "make-invoice-predictions = hello_world.main:main"
